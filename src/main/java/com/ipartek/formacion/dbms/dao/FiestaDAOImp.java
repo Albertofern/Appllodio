a32b8c8e12251fe52668fd0df0bb1569686fa0ce
Curso <Curso@Formacion-TOSH1.ipartekaula.com>
2017-04-25 15:57:50 +0200

    FiestaDAOImp

--- src/main/java/com/ipartek/formacion/dbms/dao/FiestaDAOImp.java
+++ src/main/java/com/ipartek/formacion/dbms/dao/FiestaDAOImp.java
@@ -4,6 +4,7 @@
 package com.ipartek.formacion.dbms.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import javax.sql.DataSource;
 
@@ -11,10 +12,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcCall;
 import org.springframework.stereotype.Repository;
 
 import com.ipartek.formacion.dbms.dao.interfaces.FiestaDAO;
+import com.ipartek.formacion.dbms.mappers.FiestaMapper;
 import com.ipartek.formacion.dbms.persistence.Fiesta;
 
 /**
@@ -27,6 +33,8 @@ public class FiestaDAOImp implements FiestaDAO {
 	@Autowired
 	@Qualifier("mysqlDataSource")
 	private DataSource dataSource;
+	private JdbcTemplate jdbctemplate;
+	private SimpleJdbcCall jdbcCall;
 
 	private Logger logger = LoggerFactory.getLogger(FiestaDAOImp.class);
 
@@ -35,36 +43,78 @@ public class FiestaDAOImp implements FiestaDAO {
 	@Override
 	public void setDataSource(DataSource dataSource) {
 		this.dataSource = dataSource;
+		this.jdbctemplate = new JdbcTemplate(dataSource);
 	}
 
 	@Override
 	public Fiesta create(Fiesta fiesta) {
+		final String SQL = "fiestaCreate";
+		this.jdbcCall = new SimpleJdbcCall(dataSource);
+		jdbcCall.withProcedureName(SQL);
+		SqlParameterSource in = new MapSqlParameterSource()
+				.addValue("pnombre", fiesta.getNombre())
+				.addValue("pdescripcion", fiesta.getDescripcion())
+				.addValue("pfecha", fiesta.getFecha());	
+		logger.info("codigo barrio:"+fiesta.getBarrio().getCodigo());
 		logger.info("METHOD DAO: getAll() -- PARAMS: " + fiesta.toString());
-		return null;
+		Map<String, Object> out = jdbcCall.execute(in);
+		fiesta.setCodigo((Integer) out.get("pcodigo"));
+		return fiesta;
 	}
 
 	@Override
 	public List<Fiesta> getAll() {
-		// TODO Auto-generated method stub
-		return null;
+		final String SQL = "CALL fiestagetAll";
+		List<Fiesta> fiestas = null;
+		try {
+			fiestas = jdbctemplate.query(SQL, new FiestaMapper());
+			logger.info("METHOD DAO: getAll() -- LIST SIZE: " + String.valueOf(fiestas.size()));
+		} catch (EmptyResultDataAccessException e) {
+			logger.trace("NO DATA:  " + e.getMessage() + " " + SQL);
+			// fiestas= new ArrayList<Fiesta>();
+		}
+		return fiestas;
 	}
 
 	@Override
 	public Fiesta getById(int codigo) {
-		// TODO Auto-generated method stub
-		return null;
+		Fiesta fiesta = null;
+		final String SQL = "CALL fiestagetById(?)";
+		try {
+			fiesta = jdbctemplate.queryForObject(SQL, new FiestaMapper(), new Object[] { codigo });
+			logger.info("METHOD DAO: getById() -- PARAMS:  " + fiesta.toString());
+		} catch (EmptyResultDataAccessException e) {
+			fiesta = null;
+			logger.info("NO DATA:  " + codigo + " " + e.getMessage());
+		}
+		return fiesta;
 	}
 
 	@Override
 	public Fiesta update(Fiesta fiesta) {
-		// TODO Auto-generated method stub
-		return null;
+		final String SQL = "fiestaUpdate";
+
+		this.jdbcCall = new SimpleJdbcCall(dataSource);
+		jdbcCall.withProcedureName(SQL);
+		SqlParameterSource in = new MapSqlParameterSource().addValue("pcodigo", fiesta.getCodigo())
+				.addValue("pnombre",fiesta.getNombre())
+				.addValue("pdescripcion",fiesta.getDescripcion())
+				.addValue("pfecha",fiesta.getFecha());
+
+		logger.info("METHOD DAO: update() -- PARAMS:  " + fiesta.toString());
+		jdbcCall.execute(in);
+
+		return fiesta;
 	}
 
 	@Override
 	public void delete(int codigo) {
-		// TODO Auto-generated method stub
-		
+		String SQL = "fiestaDelete";
+		this.jdbcCall = new SimpleJdbcCall(dataSource);
+		jdbcCall.withProcedureName(SQL);
+		SqlParameterSource in = new MapSqlParameterSource().addValue("pcodigo", codigo);
+		logger.info("METHOD DAO: delete()");
+		jdbcCall.execute(in);
 	}
 
 }
